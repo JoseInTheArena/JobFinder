4dc6ff06d6c4d3af9b430f3bc52a19f72b32b408
Jose Arzuaga <arzuaga@gmail.com>
2015-04-27 08:32:55 -0400

    Added mongo support from MongoLabs

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var jobModel = require("./models/Job");
 
 var app = express();
 
@@ -7,10 +9,28 @@ app.set("view engine", "jade");
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/api/jobs", function(req, res){
+    mongoose.model("Job").find({}).exec(function(error, collection){
+		res.send(collection);
+	});
+});
+
 app.get("*", function(req, res){
     res.render("index");
 });
 
-//This setting works for Cloud9
-//app.listen(3000);
-app.listen(process.env.PORT, process.env.IP);
+//mongoose.connect("mongodb://localhost/jobfinder");
+mongoose.connect("mongodb://root:p0r!v071@ds053937.mongolab.com:53937/jobfinder");
+
+var con = mongoose.connection;
+con.once("open", function(){
+	console.log("Connected to MongoDB!");
+	jobModel.seedJobs();
+});
+
+if(process.env.PORT){
+	app.listen(process.env.PORT, process.env.IP);	
+}
+else{
+	app.listen(3000);
+}
